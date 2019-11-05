0b81591a73fd5f112fd46f99ddb59d3e3e6c497c
bk8200 <logitech567@gmail.com>
2019-11-05 09:35:52 +0000

    sledi željeni točki

--- src/amswmr/scripts/wmrros.py
+++ src/amswmr/scripts/wmrros.py
@@ -6,9 +6,11 @@ import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String, Int64
-from math import cos, sin
+from math import cos, sin, atan2
 
-
+def wrapToPi(kot):
+  kot = atan2(sin(kot),cos(kot))
+  return kot  
 
 
 class WmrRos(Wmr):
@@ -25,7 +27,9 @@ class WmrRos(Wmr):
     self.y = 0.0
     self.fi = 0.0
     self.old_error = 0.0
-    
+    self.gama = 0.0
+
+  
   def _handleCmdVel(self, msg):
     self.setVel(msg.linear.x, msg.angular.z)
     
@@ -39,7 +43,8 @@ class WmrRos(Wmr):
       self.encoderROld = right
 
     max_gama = 8191.0
-    gama = - (heading - 4320.0) * 2.0 * pi / max_gama
+    self.gama = - (heading - 4320.0) * 2.0 * pi / max_gama
+    
     
     dl = -(left - self.encoderLOld)
     dr = right - self.encoderROld
@@ -51,19 +56,19 @@ class WmrRos(Wmr):
     
     d = d / konst
     
-    self.x = self.x + d*cos(gama)*cos(self.fi)
-    self.y = self.y + d*cos(gama)*sin(self.fi)
-    self.fi = self.fi + d*sin(gama) / D
+    self.x = self.x + d*cos(self.gama)*cos(self.fi)
+    self.y = self.y + d*cos(self.gama)*sin(self.fi)
+    self.fi = self.fi + d*sin(self.gama) / D
 
     #TODO Implement odometry
     
     self.encoderLOld = left
     self.encoderROld = right 
             
-    #print(self.x, heading, 360*gama/(2*pi), d)       
+    #print(self.x, heading, 360*self.gama/(2*pi), d)       
     #print( heading)
-    #print(self.x, self.y, 360*self.fi/(2*pi), 360*gama/(2*pi), heading)
-    #print(left, right, heading, 360*gama/(2*pi))
+    #print(self.x, self.y, 360*self.fi/(2*pi), 360*self.gama/(2*pi), heading)
+    #print(left, right, heading, 360*self.gama/(2*pi))
     
     
     
@@ -105,7 +110,7 @@ class WmrRos(Wmr):
         print('Levo kolo:' + str(vv))
         '''
 
-    sled_rob = 1# levi rob -1, desni rob 1
+    sled_rob = 0# levi rob -1, desni rob 1, sledi točki 0
     
     if sled_rob == -1:
       line_error = left;
@@ -113,8 +118,50 @@ class WmrRos(Wmr):
       
     elif sled_rob == 1:
       line_error = right;
+
+      
+    if sled_rob == 0:
+      x_ref = 0.5
+      y_ref = 0.5
+      
+      dist_to = sqrt((self.x -x_ref)**2+(self.y -y_ref)**2)
+      
+      fi_ref = atan2((y_ref-self.y),(x_ref-self.x))
+      
+      e_fi = wrapToPi(fi_ref - self.fi)
+      
+      #print(str(dist_to))
+      
+      v_max = 0.3
+      Kw = 0.5
+      Kgama = 0.2
+      Kv = 4.0
+      dist_min = 0.2
+      brake = sqrt(dist_to/dist_min)
+      if brake > 1:
+        brake = 1
+      
+      print(str(brake))
+      
+      ws = brake*Kw * (e_fi) - Kgama * self.gama
+      vs = brake*Kv * dist_to / cos(self.gama)
+      
+      vs = v_max * vs / (abs(vs)+abs(ws))
+      
+      
+      self.setVel(vs, ws)
+           
+      
+      
+      
+      
+      
+      
+      
+      
+      
     
-    if left == None or right == None:
+    elif left == None or right == None:
       print('None')
     else:
         
