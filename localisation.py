26303f7d87c4d5dbfca4022424c5642c94ae9724
Blaz <kosenina.blaz@gmail.com>
2019-12-24 12:02:03 +0100

    kalman ready

--- localisation.py
+++ localisation.py
@@ -7,120 +7,104 @@ from geometry_msgs.msg import TransformStamped
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Int64
 import numpy as np
-#from ams import wrapToPi
-#from world import MTAG
-#import graph_gen as graph
-#import tf.transformations as tft
+
+
+# from ams import wrapToPi
+# from world import MTAG
+# import graph_gen as graph
+# import tf.transformations as tft
 
 class Localisation(object):
-  def __init__(self):
-    self._tfBroadcaster = tf2_ros.TransformBroadcaster()
-    
-    self._subOdom = rospy.Subscriber('odom', Odometry, self._handleOdometry)
-    self._subTag = rospy.Subscriber('tag', Int64, self._handleTag)
-    self.Q = np.matrix('1 0; 0 1')
-    self.R = np.matrix('1 0 0; 0 1 0; 0 0 1')
-    self.P = np.matrix('10 0 0; 0 10 0; 0 0 10')
-    
-    
-  def _handleTag(self, msg):
-    print('Tag data: {}'.format(msg.data))
-    
-  def _handleOdometry(self, msg):      
-    #TODO Implement localisation algorithm ...
-    x, y, phi = 0.0, 0.0, 0.0
-    D = 0.1207
-    
-    delta_d = msg.twist.twist.linear.x
-    gama = msg.twist.twist.angular.z 
-    
-    dq_0 = np.matrix('delta_d * cos(gama) * cos(phi); delta_d * cos(gama) * sin(phi); delta_d * sin(gama) / D;')
-    
-    # Odometrija
-    q_1 = q_0 + delta_d * dq_0
-    
-    
-    A_curr = np.matrix('1 0 -delta_d*sin(phi)*cos(gama);
-                        0 1  delta_d*cos(phi)*cos(gama);
-                        0 0                           1')
-                        
-    F_curr = np.matrix('cos(gama)*cos(phi) -delta_d*sin(gama)*cos(phi);
-                        cos(gama)*sin(phi) -delta_d*sin(gama)*sin(phi);
-                        sin(gama)/D         delta_d*cos(gama)/D')
-                        
-    C = np.eye(3)
-    CT = np.transpose(C)
-    
-    
-    # Predikcija
-    
-    Pk_k1 = A_curr * Pk1_k1 * np.transpose(A_curr) + F_curr * self.Q * np.transpose(F_curr)
-    
-    
-    # Korekcija
-    K = Pk_k1 * CT * np.inv(C * Pk_k1 * CT + R)
-    
-    q_1_ocena = q_0_ocena + K * (z - z_ocena)
-    
-    Pkk = Pk_k1 - K * C * Pk_k1
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #'''
-    trans = TransformStamped()
-    trans.header.stamp = rospy.Time.now()
-    trans.header.frame_id = 'world'
-    trans.child_frame_id = 'NS/estimate'
-    trans.transform.translation.x = x
-    trans.transform.translation.y = y
-    trans.transform.rotation.z = sin(phi/2.0)
-    trans.transform.rotation.w = cos(phi/2.0)
-    self._tfBroadcaster.sendTransform(trans)
-    '''
-    vecTodom2wmr = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z])
-    vecQodom2wmr = np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-    vecTworld2wmr = np.array([x, y, 0.0])
-    vecQworld2wmr = np.array([0.0, 0.0, sin(phi/2.0), cos(phi/2.0)])
-    vecQworld2odom = tft.quaternion_multiply(vecQworld2wmr, tft.quaternion_conjugate(vecQodom2wmr))
-    matRworld2odom = tft.quaternion_matrix(vecQworld2odom)[0:3,0:3]
-    vecTworld2odom = vecTworld2wmr - matRworld2odom.dot(vecTodom2wmr)
-    msgQworld2odom = Quaternion(*vecQworld2odom)
-    msgTworld2odom = Point(*vecTworld2odom)
-    
-    trans2 = TransformStamped()
-    trans2.header.stamp = msg.header.stamp
-    trans2.header.frame_id = 'world'
-    trans2.child_frame_id = 'NS/odom'
-    trans2.transform.translation = msgTworld2odom
-    trans2.transform.rotation = msgQworld2odom
-    
-    transMsg = TransformStamped()
-    transMsg.header = msg.header
-    transMsg.child_frame_id = msg.child_frame_id
-    transMsg.transform.translation = msg.pose.pose.position
-    transMsg.transform.rotation = msg.pose.pose.orientation
-    
-    self._tfBroadcaster.sendTransform([trans2, transMsg])
-    #'''
-    
+    def __init__(self):
+        self._tfBroadcaster = tf2_ros.TransformBroadcaster()
+
+        self._subOdom = rospy.Subscriber('odom', Odometry, self._handleOdometry)
+        self._subTag = rospy.Subscriber('tag', Int64, self._handleTag)
+        self.tag = 0
+
+        self.P = np.array([[10, 0, 0], [0, 10, 0], [0, 0, 10]])
+        self.C = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        self.Q = np.array([[1, 0], [0, 1]])
+        self.R = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    def _handleTag(self, msg):
+        self.tag = msg.data
+        print('Tag data: {}'.format(msg.data))
+
+    def _handleOdometry(self, msg):
+        D = 0.1207
+        # x, y, phi = 0.0, 0.0, 0.0   #  x, y in phi dobimo iz handleTag?
+        delta_d = msg.twist.twist.linear.x
+        gama = msg.twist.twist.angular.z
+
+        A = np.array([[1, 0, -delta_d * sin(phi) * cos(gama)], [0, 1, delta_d * cos(phi) * cos(gama)], [0, 0, 1]])
+        F = np.array([[cos(phi) * cos(gama), -delta_d * cos(phi) * sin(gama)],
+                      [sin(phi) * cos(gama), -delta_d * sin(phi) * sin(gama)],
+                      [1 / 0.125 * sin(gama), delta_d / 0.125 * cos(gama)]])
+
+        A_T = np.transpose(A)
+        F_T = np.transpose(F)
+
+        #### Predikcija ####
+
+        # Odometrija
+        dq_0 = np.array([delta_d * cos(gama) * cos(phi)], [delta_d * cos(gama) * sin(phi)], [delta_d * sin(gama) / D])
+        q = q_0 + delta_d * dq_0  # ?????
+        x = A * q
+        P = A * self.P * A_T + F * self.Q * F_T
+
+        #### Korekcija ####
+        K1 = np.dot(self.P, self.C)
+        K2 = np.inv(
+            np.dot(self.C, np.dot(self.P, self.C)) + self.R)  # preveri če je množenje matrik v pravilnem vrstnem redu
+        K = np.dot(K1, K2)
+
+        # Zadnji prebran tag
+        z = self.tag  # Preveri ali vsebuje samo ID taga ali tudi pozicijo taga
+
+        x_ocena = (z - x)
+        x = x + np.dot(K, x_ocena)
+        self.P = self.P - np.dot(self.K, np.dot(self.C, self.P))
+
+        # '''
+        trans = TransformStamped()
+        trans.header.stamp = rospy.Time.now()
+        trans.header.frame_id = 'world'
+        trans.child_frame_id = 'NS/estimate'
+        trans.transform.translation.x = x
+        trans.transform.translation.y = y
+        trans.transform.rotation.z = sin(phi / 2.0)
+        trans.transform.rotation.w = cos(phi / 2.0)
+        self._tfBroadcaster.sendTransform(trans)
+        '''
+        vecTodom2wmr = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z])
+        vecQodom2wmr = np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+        vecTworld2wmr = np.array([x, y, 0.0])
+        vecQworld2wmr = np.array([0.0, 0.0, sin(phi/2.0), cos(phi/2.0)])
+        vecQworld2odom = tft.quaternion_multiply(vecQworld2wmr, tft.quaternion_conjugate(vecQodom2wmr))
+        matRworld2odom = tft.quaternion_matrix(vecQworld2odom)[0:3,0:3]
+        vecTworld2odom = vecTworld2wmr - matRworld2odom.dot(vecTodom2wmr)
+        msgQworld2odom = Quaternion(*vecQworld2odom)
+        msgTworld2odom = Point(*vecTworld2odom)
+
+        trans2 = TransformStamped()
+        trans2.header.stamp = msg.header.stamp
+        trans2.header.frame_id = 'world'
+        trans2.child_frame_id = 'NS/odom'
+        trans2.transform.translation = msgTworld2odom
+        trans2.transform.rotation = msgQworld2odom
+
+        transMsg = TransformStamped()
+        transMsg.header = msg.header
+        transMsg.child_frame_id = msg.child_frame_id
+        transMsg.transform.translation = msg.pose.pose.position
+        transMsg.transform.rotation = msg.pose.pose.orientation
+
+        self._tfBroadcaster.sendTransform([trans2, transMsg])
+        #'''
+
+
 if __name__ == '__main__':
-  rospy.init_node('localisation')
-  localisation = Localisation()
-  rospy.spin()
+    rospy.init_node('localisation')
+    localisation = Localisation()
+    rospy.spin()
